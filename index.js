662ae5f8b513234dc1503bf9871f99d16fb2ff80
Bharti Sagar <bhartisagar93.bs@gmail.com>
2025-03-25 06:30:26 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const connectToMongo = require("./db");
+
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+connectToMongo();
+
+app.listen(port, () => {
+    console.log(`app listening on https://localhost:${port}`);
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/policy", require("./routes/policyRoutes"));
+app.use("/api/orders", require("./routes/ordersRoutes"));
